918645b81822f71821dfce544ea5be0abf50170e
grvmourya <gauravmourya1999@gmail.com>
2022-03-06 02:25:32 +0530

    added zoom handler

---
+++ Assets/LOI/Scripts/Base/ZoomControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomControl : MonoBehaviour
+{
+    public float ZoomChange;
+    public float SmoothChange;
+    public float MinSize, MaxSize;
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+            cam.orthographicSize -= ZoomChange * Time.deltaTime * SmoothChange;
+        if (Input.mouseScrollDelta.y < 0)
+            cam.orthographicSize += ZoomChange * Time.deltaTime * SmoothChange;
+
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, MinSize, MaxSize);
+    }
+}
